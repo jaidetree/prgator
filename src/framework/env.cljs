b1050f02fd19b7b789369d434c0645ab891516f6
Jay <jayzawrotny@gmail.com>
2023-07-25 19:06:55 -0400

    Make env file optional

--- src/framework/env.cljs
+++ src/framework/env.cljs
@@ -6,12 +6,14 @@
 
 (defn parse-ini-line
   [ini-line]
-  (loop [remaining ini-line
-         name ""]
-    (let [[c & remaining] remaining]
-      (case c
-        "=" (str "[:" name " " (s/join "" remaining) "]")
-        (recur remaining (str name c))))))
+  (if (s/blank? ini-line)
+    ""
+    (loop [remaining ini-line
+           name ""]
+      (let [[c & remaining] remaining]
+        (case c
+          "=" (str "[:" name " " (s/join "" remaining) "]")
+          (recur remaining (str name c)))))))
 
 (defn wrap-pairs
   [pairs-str]
@@ -22,7 +24,7 @@
   (try
     (read-file filename)
     (catch js/Error error
-      (js/console.error error)
+      #_(js/console.error error)
       "")))
 
 (def env
@@ -45,6 +47,6 @@
 (defn required
   [key]
   (let [value (get env (keyword key) ::not-found)]
-    (if (= value ::not-found)
+    (when (= value ::not-found)
       (throw (js/Error. (str "Required: Could not find ENV var " key))))
-      value))
+    value))
